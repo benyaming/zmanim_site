4daa98ec9c35d67b7619f4cf4132b82194277f2a
vorant94 <vorant94@gmail.com>
2022-01-08 10:16:02 +0200

    after ng update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -55,7 +55,7 @@
 /***************************************************************************************************
  * Zone JS is required by default-layout for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
