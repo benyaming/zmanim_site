1f9c0a5f8a121e7ec5759b5f12add5c319803f40
bonusrk <word0bearer@gmail.com>
2022-02-20 16:33:22 +0300

    feat: day-by-day switch controls for date-picker

--- src/app/core/zmanim/zmanim.dtos.ts
+++ src/app/core/zmanim/zmanim.dtos.ts
@@ -20,7 +20,7 @@ export interface ZmanimZmanimRequestDto {
   readonly astronomical_hour_gra?: boolean;
 }
 
-export interface ZmanimZmanimResponseDto {
+export interface ZmanimZmanimResponseDto extends Readonly<Record<string, any>> {
   readonly settings?: ZmanimSettingsResponseDto;
   readonly alos: string | null;
   readonly sunrise: string | null;
