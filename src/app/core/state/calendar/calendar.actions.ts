ee50553d015d42c29becae614f133ef4e3741ca3
vorant94 <vorant94@gmail.com>
2022-03-11 18:33:58 +0200

    fixed calendar action type strings

--- src/app/core/state/calendar/calendar.actions.ts
+++ src/app/core/state/calendar/calendar.actions.ts
@@ -2,17 +2,17 @@ import { NavigationDirection } from '@shared/types';
 import { CalendarDayModel } from './calendar.models';
 
 export class NavigateCalendar {
-  static readonly type = '[App] Navigate calendar';
+  static readonly type = '[Calendar] Navigate calendar';
 
   constructor(public readonly payload: NavigationDirection) {}
 }
 
 export class SelectCalendarDay {
-  static readonly type = '[App] Select calendar day';
+  static readonly type = '[Calendar] Select calendar day';
 
   constructor(public readonly payload: CalendarDayModel) {}
 }
 
 export class GenerateCalendarDays {
-  static readonly type = '[App] Generate calendar days';
+  static readonly type = '[Calendar] Generate calendar days';
 }
