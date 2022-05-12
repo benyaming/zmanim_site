7056816cbb3d039e5162f499dbefeed590f41489
bonusrk <word0bearer@gmail.com>
2022-05-12 08:43:47 +0300

    feat: calendar navigation

---
+++ src/components/domain/index.ts
@@ -0,0 +1,1 @@
+export { ZmanimCalendar } from './zmanimCalendar';
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,15 @@
-import { Box, Flex } from '@chakra-ui/react';
-import { getTime } from 'date-fns';
-import React, { useState } from 'react';
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
 
-import { ZmanimCalendar } from '../components/domain/calendar/ZmanimCalendar';
-import { ZmanimInfo } from '../components/domain/zmanim/ZmanimInfo';
+import { ZmanimCalendar } from '../components';
+import { CalendarProvider } from '../providers/CalendarProvide';
 
 export const Home = () => {
-  const [timestamp, setTimestamp] = useState(0);
-  const handleDayClick = (v: Date) => {
-    setTimestamp(getTime(v));
-  };
   return (
-    <Flex gap={4}>
-      <Box w="80%">
-        <ZmanimCalendar handleDayClick={handleDayClick} />
-      </Box>
-      <Box>
-        <ZmanimInfo timestamp={timestamp} />
-      </Box>
+    <Flex justifyContent="center">
+      <CalendarProvider>
+        <ZmanimCalendar />
+      </CalendarProvider>
     </Flex>
   );
 };
